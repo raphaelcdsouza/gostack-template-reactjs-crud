0a1c25de79defc0bba4c112de6e5e0e5bdcfa8de
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-08-10 14:32:32 -0300

    Feature edit food plate

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -60,7 +60,11 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const { id, available } = editingFood;
+
+    const response = await api.put(`/foods/${id}`, {...food, id, available});
+
+    setFoods(oldState => oldState.map(food => food.id === id ? response.data : food));
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
@@ -78,7 +82,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
